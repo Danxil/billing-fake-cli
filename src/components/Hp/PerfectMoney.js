97796a86a71ee906e7209122012dcbfad57824a2
Vadym Bondarenko <danxil@list.ru>
2019-03-20 21:23:09 +0200

    Added PM

--- src/components/Hp/PerfectMoney.js
+++ src/components/Hp/PerfectMoney.js
@@ -21,7 +21,7 @@ const CoinPayments = ({
     <input type="text" name="PAYMENT_AMOUNT" value={amount} />
     <input type="hidden" name="PAYMENT_UNITS" value="USD" />
     <input type="hidden" name="STATUS_URL" value={`http://www.cases-billing.live/perfect-money/info/`} />
-    <input type="hidden" name="PAYMENT_URL" value={`http://www.cases-billing.live/perfect-money/success/?merchant=${merchant}`} />
+    <input type="hidden" name="PAYMENT_URL" value={`http://www.cases-billing.live/perfect-money/success/`} />
     <input type="hidden" name="PAYMENT_URL_METHOD" value="POST" />
     <input type="hidden" name="NOPAYMENT_URL" value="http://www.cases-billing.live/perfect-money/fail/" />
     <input type="hidden" name="NOPAYMENT_URL_METHOD" value="POST" />
